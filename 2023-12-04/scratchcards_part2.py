36945b125e0828d9c90ca9cdb30e4cba10556267
cybersnacker <dimpy_1991@yahoo.co.in>
2023-12-04 09:23:25 -0500

    Use walrus operator

--- 2023-12-04/scratchcards_part2.py
+++ 2023-12-04/scratchcards_part2.py
@@ -11,10 +11,10 @@ for e in entries:
     winning, actual = lst.split('|')
     winning = re.findall(r'\d+', winning)
     actual = re.findall(r'\d+', actual)
-    if len(set(winning) & set(actual)) > 0:
+    if (num_winning:= len(set(winning) & set(actual))) > 0:
         #Every copy of the card wins the number of matches, hence add subsequent card instances by multipler
         multiplier = card_instances[id]
-        for i in range(1, len(set(winning) & set(actual))+1):
+        for i in range(1, num_winning+1):
             card_instances[id+i] += (1 * multiplier)
 
 print("Total card instance: ", sum(card_instances.values()))
